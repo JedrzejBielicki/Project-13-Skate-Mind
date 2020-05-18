e154349b04caae23df6992d8eb47a901afe4c6ee
Jędrzej Bielicki <jedrzejb123@gmail.com>
2020-05-18 13:44:02 +0200

    style progres

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-
-
 const parkPic = document.querySelector(".park-img");
-console.log(parkPic)
+const aboutSection = document.querySelector(".about");
+const mainSection = document.querySelector(".main");
+console.log(scrollY);
+document.addEventListener('scroll', function(){
+    if(scrollY> mainSection.offsetHeight / 3){
+        parkPic.classList.add('pic-animation');
+    }
+    else{
+        parkPic.classList.remove('pic-animation');
+    }
+})
