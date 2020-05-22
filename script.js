e4da1e08732c9197d4d265c4ace16253ffbea907
Jędrzej <jedrzejb123@gmail.com>
2020-05-22 12:04:49 +0200

    add map section

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const parkPic = document.querySelector(".park-img");
-const aboutSection = document.querySelector(".about");
 const mainSection = document.querySelector(".main");
-console.log(scrollY);
-document.addEventListener('scroll', function(){
-    if(scrollY> mainSection.offsetHeight / 3){
+const learnMoreBtn = document.querySelector(".learn-more")
+
+
+document.addEventListener('scroll', function () {
+    if (scrollY > mainSection.offsetHeight / 3) {
         parkPic.classList.add('pic-animation');
-    }
-    else{
+    } else {
         parkPic.classList.remove('pic-animation');
     }
 })
