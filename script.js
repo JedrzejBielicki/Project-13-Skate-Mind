540f442b7ff267f00c3933291f50d2fde78570f0
Jędrzej Bielicki <jedrzejb123@gmail.com>
2020-05-29 13:03:35 +0200

    fix about animation

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const parkPic = document.querySelector(".park-img");
 const mainSection = document.querySelector(".main");
+const aboutSection = document.querySelector(".about");
 const learnMoreBtn = document.querySelector(".learn-more")
 
 
 document.addEventListener('scroll', function () {
-    if (scrollY > mainSection.offsetHeight / 3) {
+    if (scrollY > mainSection.offsetHeight / 3 && scrollY < mainSection.offsetHeight + aboutSection.offsetHeight) {
         parkPic.classList.add('pic-animation');
     } else {
         parkPic.classList.remove('pic-animation');
