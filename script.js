3c7656f1afdf412cb7a38385d82d708853889f9d
Jędrzej <jedrzejb123@gmail.com>
2020-06-30 18:26:51 +0200

    add preload

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const mainSection = document.querySelector(".main");
 const aboutSection = document.querySelector(".about");
 const learnMoreBtn = document.querySelector(".learn-more")
 
+window.addEventListener('load', () => {
+    const preloader = document.querySelector('.preloader');
+    const preloaderText = document.querySelector('.preloader__text')
+    preloader.classList.add('preloader--up');
+    preloaderText.classList.add('preloader__text--up');
+})
 
 document.addEventListener('scroll', function () {
     if (scrollY > mainSection.offsetHeight / 3 && scrollY < mainSection.offsetHeight + aboutSection.offsetHeight) {
